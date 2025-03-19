2915c25bc37ebbb626c33a3944970d5a5c522809
Zheyuan Fu <accelerator0126@gmail.com>
2025-03-19 12:25:09 +0100

    sopra-fs25-group-10

--- src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
@@ -21,7 +21,7 @@ public class Application {
   @ResponseStatus(HttpStatus.OK)
   @ResponseBody
   public String helloWorld() {
-    return "The application is running.";
+    return "The application of MapMaster is running.";
   }
 
   @Bean
