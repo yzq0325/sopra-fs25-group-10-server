ab6b1467fe5dad7a51623321df394ed880e45777
Zheyuan Fu <accelerator0126@gmail.com>
2025-04-05 14:12:08 +0200

    #47 extend DTOS of  GamePostDTO and DTOImapper to receive the information from front end

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/GamePostDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/GamePostDTO.java
@@ -4,7 +4,28 @@ import java.util.List;
 
 public class GamePostDTO {
 
-    private List<String> players;        
+    private String gameName;
+
+    private List<String> players;
+    
+    private int playersNumber;
+
+    private int time;
+  
+    private String modeType;
+
+    private String lockType;
+
+    private String password;
+
+
+    public String getGameName() {
+        return gameName;
+      }
+    
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
 
     public List<String> getPlayers() {
         return players;
@@ -13,4 +34,48 @@ public class GamePostDTO {
     public void setPlayers(List<String> players) {
         this.players = players;
     }
+
+    public int getPlayersNumber() {
+        return playersNumber;
+      }
+    
+      public void setPlayersNumber(int playersNumber) {
+        this.playersNumber = playersNumber;
+      }
+      
+      public int getTime() {
+        return time;
+      }
+    
+      public void setTime(int time) {
+        this.time = time;
+      }
+
+    
+      public String getModeType() {
+        return modeType;
+      }
+    
+      public void setModeType(String modeType) {
+        this.modeType = modeType;
+      }
+    
+      public String getLockType() {
+        return lockType;
+      }
+    
+      public void setLockType(String lockType) {
+        this.lockType = lockType;
+      }
+    
+      public String getPassword() {
+        return password;
+      }
+    
+      public void setPassword(String password) {
+        this.password = password;
+      }
+    
+
+
 }
