3d9d23ed5b4eaa989a5fab535f16f72a4a8b1031
Zheyuan Fu <accelerator0126@gmail.com>
2025-04-06 13:52:06 +0200

    #61 integrate GameLobbyGetDTO into GameGetDTO, change Rest APIname to "lobby"

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameController.java
@@ -2,7 +2,6 @@ package ch.uzh.ifi.hase.soprafs24.controller;
 
 import ch.uzh.ifi.hase.soprafs24.entity.Game;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.GameGetDTO;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.GameLobbyGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.GamePostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.GameService;
@@ -31,22 +30,22 @@ public class GameController {
   @ResponseStatus(HttpStatus.CREATED)
   @ResponseBody
   public GameGetDTO createGame(@RequestBody GamePostDTO gamePostDTO) {
-      Game gameToCreate = DTOMapper.INSTANCE.convertGamePostDTOtoEntity(gamePostDTO);
+      Game gameToCreate = DTOMapper.INSTANCE.convertGamePostDTOtoGameEntity(gamePostDTO);
 
       Game createdGame = gameService.createGame(gameToCreate);
 
-      return DTOMapper.INSTANCE.convertEntityToGameGetDTO(createdGame);
+      return DTOMapper.INSTANCE.convertGameEntityToGameGetDTO(createdGame);
   }
 
-  @PostMapping("/gamelobby")
+  @PostMapping("/lobby")
   @ResponseStatus(HttpStatus.OK)
   @ResponseBody
-  public List<GameLobbyGetDTO> getGameLobby() {
+  public List<GameGetDTO> getGameLobby() {
       List<Game> allGames = gameService.getAllGames();
 
-      List<GameLobbyGetDTO> gameLobbyGetDTOs = new ArrayList<>();
+      List<GameGetDTO> gameLobbyGetDTOs = new ArrayList<>();
       for (Game game : allGames) {
-        gameLobbyGetDTOs.add(DTOMapper.INSTANCE.convertGameEntitytoGameLobbyGetDTO(game));
+        gameLobbyGetDTOs.add(DTOMapper.INSTANCE.convertGameEntityToGameGetDTO(game));
     }
     
     return gameLobbyGetDTOs;
--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/GameGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/GameGetDTO.java
@@ -6,6 +6,54 @@ public class GameGetDTO {
 
     private Map<String, String> scoreBoard;
 
+    private String gameName;
+
+    private String lockType;
+
+    private int playersNumber;
+
+    private int realPlayersNumber;
+
+    public String getGameName() {
+        return gameName;
+      }
+    
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public int getPlayersNumber() {
+        return playersNumber;
+      }
+    
+    public void setPlayersNumber(int playersNumber) {
+        this.playersNumber = playersNumber;
+      }     
+    
+    public String getLockType() {
+        return lockType;
+    }
+    
+    public void setLockType(String lockType) {
+        this.lockType = lockType;
+    }
+
+    public int getPlayerNumbers() {
+        return playersNumber;
+      }
+    
+    public void setPlayerNumbers(int playersNumber) {
+        this.playersNumber = playersNumber;
+      }     
+
+      public int getRealPlayersNumber() {
+        return realPlayersNumber;
+      }
+    
+    public void setRealPlayersNumber(int realPlayersNumber) {
+        this.realPlayersNumber = realPlayersNumber;
+      }   
+
     public Map<String, String> getScoreBoard() {
         return scoreBoard;
     }
