56011d63668dac30bb31d809728b072395bd806b
yzq0325 <ziqi.yang@uzh.ch>
2025-04-24 01:10:27 +0200

    [fix #104] modify WebSocketConfig to support XHR

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
               //after development, this will be set to real URL.
-              .setAllowedOriginPatterns("*")
+              .setAllowedOriginPatterns("http://localhost:3000", "https://sopra-fs25-group-10-client.vercel.app/")
               .withSockJS();
     }
 }
