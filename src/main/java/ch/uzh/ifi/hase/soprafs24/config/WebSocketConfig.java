4231a49eb5c7414a1321a63a6e42f590bba3be4f
yzq0325 <ziqi.yang@uzh.ch>
2025-05-27 23:51:40 +0200

    connect to vercel

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000", "https://sopra-fs25-group-10-client.vercel.app");
+                .setAllowedOrigins("http://localhost:3000", "https://sopra-fs25-group-10-client-teal.vercel.app");
                 // .withSockJS();
     }
 }
